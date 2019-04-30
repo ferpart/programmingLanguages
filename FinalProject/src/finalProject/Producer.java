ea82f1b8c2ccb9f1f36fe3825bdfe939b61ee695
HumbertoM10 <humberto_e10@hotmail.es>
2019-04-29 20:52:04 -0500

    se agrego la lista en el buffer y los sincronized

--- FinalProject/src/finalProject/Producer.java
+++ FinalProject/src/finalProject/Producer.java
@@ -7,28 +7,41 @@ import java.util.logging.Logger;
 
 public class Producer extends Thread {
     Buffer buffer;
+    String operadores;
+    int min;
+    int max;
+    int sleep;
     
-    Producer(Buffer buffer) {
+    Producer(Buffer buffer, String operadores, int min, int max, int sleep) {
         this.buffer = buffer;
+        this.operadores = operadores;
+        this.min = min;
+        this.max = max;
+        this.sleep = sleep;
     }
     
     @Override
     public void run() {
         System.out.println("Running Producer...");
-        String products = "AEIOU";
         Random r = new Random(System.currentTimeMillis());
-        char product;
+        String product;
         
-        for(int i=0 ; i<5 ; i++) {
-            product = products.charAt(r.nextInt(5));
-            this.buffer.produce(product);
-            //System.out.println("Producer produced: " + product);
-            Buffer.print("Producer produced: " + product);
-            
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+        while(true) {
+            synchronized (this){ 
+                product = "(";
+                product += operadores.charAt(r.nextInt(operadores.length())) + " ";
+                product += (r.nextInt(max-min+1)+min) + " ";
+                product += (r.nextInt(max-min+1)+min); 
+                product += ")";
+                this.buffer.produce(product);
+                //System.out.println("Producer produced: " + product);
+                Buffer.print("Producer produced: " + product);
+
+                try {
+                    Thread.sleep(sleep);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
     }
