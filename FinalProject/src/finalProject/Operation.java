f20b3c1641df867041e38b06cf5f8ea3c6f2d6a6
HumbertoM10 <humberto_e10@hotmail.es>
2019-04-29 21:10:24 -0500

    Se arreglo el nombre de la clase operacion ya que estaba en minusculas

---
+++ FinalProject/src/finalProject/Operation.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalProject;
+
+/**
+ *
+ * @author Fernando Partida
+ */
+
+
+
+public class Operation {
+    private Integer id;
+    private Integer result;
+    private String operationString;
+    
+    public Operation(String operacion){
+        
+    };
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getResult() {
+        return result;
+    }
+
+    public void setResult(Integer result) {
+        this.result = result;
+    }
+
+    public String getOperationString() {
+        return operationString;
+    }
+
+    public void setOperationString(String operationString) {
+        this.operationString = operationString;
+    }
+    
+    
+}
